4df1404fc11addcd6a3de298f20f8dd931956d21
kyuuube <benchiu@yandex.com>
2021-03-16 17:28:37 +0800

    craco 配置

--- craco.config.js
+++ craco.config.js
@@ -2,10 +2,31 @@ const SimpleProgressWebpackPlugin = require('simple-progress-webpack-plugin');
 // const logDevServerConfigPlugin = require("./craco-plugin-log-dev-server-config");
 // const reactHotReloadPlugin = require('craco-plugin-react-hot-reload')
 module.exports = {
-    // plugins: [
-    //     { plugin: logDevServerConfigPlugin, options: { preText: "Will log the dev server config:" } }
-    // ],
+    plugins: [
+        {
+            plugin: {
+                overrideDevServerConfig: ({ devServerConfig, cracoConfig, pluginOptions, context: { env, paths, proxy, allowedHost } }) => {
+                    return devServerConfig;
+                },
+            },
+            options: {},
+        },
+    ],
     webpack: {
+        devServer: {
+            /* Any devServer configuration options: https://webpack.js.org/configuration/dev-server/#devserver. */
+            compress: true,
+            open: false,
+            port: 5000,
+            // proxy: [{
+            //     context: ['/auth', '/api'],
+            //     target: apiBaseUrl,
+            //     pathRewrite: {
+            //         '^/auth': 'auth',
+            //         '^/api': 'api'
+            //     }
+            // }]
+        },
         plugins: [
             // 查看打包的进度
             new SimpleProgressWebpackPlugin(),
