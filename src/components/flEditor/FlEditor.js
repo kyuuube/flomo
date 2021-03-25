aa7ec7107fb79379c8a6e214785fba23c5990bbd
kyuuube <benchiu@yandex.com>
2021-03-25 17:15:09 +0800

    feature: 富文本编辑功能

--- src/components/flEditor/FlEditor.js
+++ src/components/flEditor/FlEditor.js
@@ -3,28 +3,54 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { Upload } from 'antd';
 import { LoadingOutlined, PlusOutlined, PictureOutlined } from '@ant-design/icons';
+import { ContentUtils } from 'braft-utils'
 import 'braft-editor/dist/index.css';
 import './index.css';
 
 function FlEditor() {
     const dispatch = useDispatch();
     let editorInstance = null;
-    let [editorState, handleChange] = useState(null);
+    let [editorState, handleChange] = useState(BraftEditor.createEditorState(null));
     // eslint-disable-next-line no-unused-vars
     let [loading, handleLoading] = useState(false);
     // eslint-disable-next-line no-unused-vars
     let [imageUrl, handleImageUrl] = useState('');
+    let [fileList, handleFileList] = useState([]);
     let content = null;
     const controls = ['list-ul', 'list-ol', 'bold', 'underline'];
+    const uploadHandler = (param) => {
+        console.log(param);
+        // if (!param.file) {
+        //     console.log(param)
+        // }
+        handleFileList([...fileList, { ...param }]);
+    };
+    const insertTag = () => {
+        console.log('insert');
+        handleChange(ContentUtils.insertText(editorState, '#'))
+    };
     const extendControls = [
         {
-            key: '  uploader',
+            key: 'custom-button',
+            type: 'button',
+            text: '#',
+            title: '标签',
+            onClick: insertTag,
+        },
+        {
+            key: 'uploader',
             type: 'component',
             component: (
-                <Upload accept="image/*" showUploadList={false} >
+                <Upload
+                    accept="image/*"
+                    fileList={fileList}
+                    action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                    onChange={() => uploadHandler()}
+                    showUploadList={false}
+                >
                     {/* 这里的按钮最好加上type="button"，以避免在表单容器中触发表单提交，用Antd的Button组件则无需如此 */}
                     <button type="button" className="control-item button upload-button" data-title="插入图片">
-                      <PictureOutlined />
+                        <PictureOutlined />
                     </button>
                 </Upload>
             ),
@@ -39,7 +65,6 @@ function FlEditor() {
         : 'absolute bottom-2 right-2 bg-green-500 text-white py-2 px-4 rounded text-xs z-50 focus:outline-none';
 
     const handleSubmitClick = () => {
-        console.log(content);
         if (!content) return;
         const memoItem = {
             id: Date.now(),
@@ -66,19 +91,37 @@ function FlEditor() {
                 extendControls={extendControls}
                 value={editorState}
                 placeholder="现在的想法是..."
-                onChange={(v) => handleChange((editorState = v))}
+                onChange={(v) => handleChange(v)}
             />
-            <div className="ml-2">
-                <Upload
-                    name="avatar"
-                    listType="picture-card"
-                    className="avatar-uploader"
-                    showUploadList={false}
-                    action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-                >
-                    {imageUrl ? <img src={imageUrl} alt="avatar" style={{ width: '100%' }} /> : uploadButton}
-                </Upload>
-            </div>
+            {/*<div className="ml-2">*/}
+            {/*    <Upload*/}
+            {/*        listType="picture-card"*/}
+            {/*        className="avatar-uploader"*/}
+            {/*        fileList={fileList}*/}
+            {/*        headers={{*/}
+            {/*            Authorization: 'F0fGTlQuhoDxEd5CYiVXpIx7Pth73AxC',*/}
+            {/*            ContentType: 'multipart/form-data',*/}
+            {/*            Origin: 'http://localhost',*/}
+            {/*            AccessControlRequestMethod: 'post',*/}
+            {/*            AccessControlRequestHeaders: 'X-Requested-With',*/}
+            {/*        }}*/}
+            {/*        action="https://sm.ms/api/v2/upload"*/}
+            {/*    >*/}
+            {/*        {fileList.length >= 4 ? null : uploadButton}*/}
+            {/*    </Upload>*/}
+            {/*</div>*/}
+            {fileList.length > 0 && (
+                <div className="ml-2">
+                    <Upload
+                        listType="picture-card"
+                        className="avatar-uploader"
+                        fileList={fileList}
+                        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                    >
+                        {fileList.length >= 4 ? null : uploadButton}
+                    </Upload>
+                </div>
+            )}
             <button onClick={() => handleSubmitClick()} className={buttonClass}>
                 发送
             </button>
